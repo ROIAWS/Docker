638f72dd9318710515de9bdbe9a71024095bfb95
Grant Moyle <Grant.A.Moyle@roitraining.com>
2021-08-25 13:35:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from waitress import serve
+import os
+
 import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def getbalance():
-  acct_balance = random.randint(1, 10000000)
-  return "Your Bank Account Balance is " + str(acct_balance) + "!\n"
+def hello():
+  acct_balance = random.randint(1, 100000000)/100
+  return "Your Bank Account Balance is $" + str(acct_balance) + "!\n"
 
 @app.route("/version")
 def version():
-  return "Demo 1.0\n"
+  return "Helloworld 1.1\n"
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  serve(app,host="0.0.0.0",port=int(os.environ.get("PORT", 8080)))
