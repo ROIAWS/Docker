3c8a75e9ae3f30470813e256da199c2db713ad36
Grant Moyle <Grant.A.Moyle@roitraining.com>
2025-09-04 14:34:26 -0500

    Update app.py

--- AWSPython/app.py
+++ AWSPython/app.py
@@ -3,8 +3,7 @@ from faker import Faker
 
 def lambda_handler(event, context):
     fake = Faker()
-    message = fake.name() + " - welcome to SY"
-    # TODO implement
+    message = fake.name() + " - Welcome to SY"
     return {
         'statusCode': 200,
         'body': json.dumps(message)
